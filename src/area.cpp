1d4c4bb2913a1d66be8fc54781ca456083365f57
jallmenroeder <jallmenroeder@gmail.com>
2020-10-20 11:32:33 +0200

    evaluate area light wrt solid angles

--- src/area.cpp
+++ src/area.cpp
@@ -1,5 +1,4 @@
 #include <nori/emitter.h>
-#include <nori/mesh.h>
 
 NORI_NAMESPACE_BEGIN
 
@@ -13,13 +12,10 @@ public:
     Color3f evalQueryRecord(const EmitterQueryRecord &record) const override {
         Vector3f x_y = record.light_point - record.shading_point;
         Vector3f l_i = (x_y).normalized();
-        Vector3f l_o = -l_i;
         float cos_theta_i = l_i.dot(record.shading_normal);
-        float cos_theta_o = l_o.dot(record.light_normal);
-        if (cos_theta_i <= 0 || cos_theta_o <= 0)
+        if (cos_theta_i <= 0)
             return {0.f};
-        float dist_2 = x_y.squaredNorm();
-        return m_radiance * cos_theta_i * cos_theta_o / dist_2;
+        return m_radiance * cos_theta_i;
     }
 
     Color3f getRadiance() const override {
