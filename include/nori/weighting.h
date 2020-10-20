4459bcb81fabfa0e40c8f767169d86eb2e77f562
jallmenroeder <jallmenroeder@gmail.com>
2020-10-20 11:30:46 +0200

    fix power heuristic

--- include/nori/weighting.h
+++ include/nori/weighting.h
@@ -20,7 +20,9 @@ public:
      * Returns weights for two sampling functions F and G according to the power heuristic. A power of 2 is used here.
      */
     static inline float power2Heuristic(int num_sample_F, float pdf_F, int num_samples_G, float pdf_G) {
-        return ((float)num_sample_F * pdf_F) / ((float)num_sample_F * pdf_F + (float)num_samples_G * pdf_G);
+        float f = (float)num_sample_F * pdf_F;
+        float g = (float)num_samples_G * pdf_G;
+        return f * f / (f * f + g * g);
     }
 };
 
